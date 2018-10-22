1cb2ecd0105b7a63d44614aff0c3b0267c730681
Koen Schaft <koen@grrr.nl>
2018-10-22 17:40:47 +0200

    Add distribution

--- dist/util.js
+++ dist/util.js
@@ -3,7 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.warn = exports.findElementWithHandler = exports.findInDomAncestry = void 0;
+exports.toArray = exports.warn = exports.findElementWithHandler = exports.findInDomAncestry = void 0;
+
+var _hansel = require("./hansel");
 
 /**
  * Generic function for traversing a DOM, returning the first element
@@ -13,7 +15,7 @@ exports.warn = exports.findElementWithHandler = exports.findInDomAncestry = void
  */
 var findInDomAncestry = function findInDomAncestry(predicate) {
   return function (node) {
-    return typeof node.nodeType === 'undefined' || node.nodeType === Node.DOCUMENT_NODE ? undefined : predicate(node) ? node : findInDomAncestry(predicate)(node.parentNode);
+    return !node || node.nodeType === Node.DOCUMENT_NODE ? undefined : predicate(node) ? node : findInDomAncestry(predicate)(node.parentNode);
   };
 };
 /**
@@ -25,7 +27,7 @@ var findInDomAncestry = function findInDomAncestry(predicate) {
 
 exports.findInDomAncestry = findInDomAncestry;
 var findElementWithHandler = findInDomAncestry(function (x) {
-  return x.hasAttribute('data-handler');
+  return x.hasAttribute(_hansel.HANDLER_ATTRIBUTE);
 });
 /**
  * console.warn proxy.
@@ -43,5 +45,17 @@ var warn = function warn(elm) {
 
   return typeof elm.ownerDocument.defaultView.console !== 'undefined' ? (_elm$ownerDocument$de = elm.ownerDocument.defaultView.console).warn.apply(_elm$ownerDocument$de, args) : undefined;
 };
+/**
+ * Crude and incomplete replacement for Array.from
+ *
+ * nodesToArray :: NodeList -> Array
+ */
+
 
 exports.warn = warn;
+
+var toArray = function toArray(xs) {
+  return Array.prototype.slice.call(xs);
+};
+
+exports.toArray = toArray;
