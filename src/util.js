46a2b3508a0527c528ca66c0478c652c414d4ebb
Koen Schaft <koen@grrr.nl>
2018-10-30 15:52:29 +0100

    Check for the existence of hasAttribute on the root element

--- src/util.js
+++ src/util.js
@@ -14,7 +14,6 @@ export const findInDomAncestry = predicate => node =>
       ? node
       : findInDomAncestry(predicate)(node.parentNode);
 
-
 /**
  * Find element with data-handler attribute.
  *
